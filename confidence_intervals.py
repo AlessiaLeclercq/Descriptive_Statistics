5d92ad0bd5176c33e69df6b55b8f6f2eb93a5e78
AlessiaLeclercq <alessialeclercq@hotmail.it>
2022-12-15 12:38:41 +0100

    separate functions from confidence_intervals

--- confidence_intervals.py
+++ confidence_intervals.py
@@ -1,20 +1,6 @@
 import math
+from functions import *
 from scipy import stats
-
-def sample_mean(sample, n):
-    return sum(sample)/n
-
-def sample_variance(sample, mean, n):
-    return [(s - mean)**2 for s in sample]/(n-1)
-    
-def compute_t_statistic(param, df):
-    return stats.t.ppf(param, df)
-
-def compute_z_statistic(param):
-    return stats.norm.ppf(param)
-
-def compute_chi_statistic(param, df):
-    return stats.chi2.ppf(param, df)
 
 def confidence_for_mean(sample_mean, variance, n, confidence, big_sample = True, true_var = True):
     '''Computes the two sided confidence intervals for the mean '''
